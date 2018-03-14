5b6551e57d9c72d907356e0a2138d62307a876d6
githubsven <s.luehof@gmail.com>
2018-03-14 14:42:15 +0100

    t

---
+++ cv_assignment4/main.cpp
@@ -0,0 +1,10 @@
+#include <opencv2\core\core.hpp>
+#include <opencv2\calib3d\calib3d.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+int main(int argc, char** argv) {
+	cv::namedWindow("H", 1);
+	cv::waitKey(10000);
+	return 0;
+}
